3e1cc8b54e858d73df2d05f75acd625a4baf2dfd
我系罗罗罗罗罗 <in@heyzh.com>
2014-06-12 14:49:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,14 +7,19 @@ query.queryArrivalForetell("2", "湾仔沙", function (err, data) {
     if (err) {
         console.log("出错啦，报错内容如下：\n" + err);
     }
-    var txt = "您正在查询 " + data.queryLineName + "  的到站情况如下：\n\n",
-        i,
-        dataLength;
-    for (i = 0, dataLength = data.foretellDetail.length; i < dataLength; i++) {
-        txt += "从 " + data.foretellDetail[i].startStationName + " → " + data.foretellDetail[i].endStationName +
-            "\n已到达 " + data.foretellDetail[i].vehicleStationName +
-            "\n还有 " + data.foretellDetail[i].distanceStationNumber + " 个站到达 " + data.queryStationName +
-            "\n-------------------------------------------------------------------------------------------\n";
+    if (data) {
+        var txt = "您正在查询 " + data.queryLineName + "  的到站情况如下：\n\n",
+            i,
+            dataLength;
+        for (i = 0, dataLength = data.foretellDetail.length; i < dataLength; i++) {
+            txt += "从 " + data.foretellDetail[i].startStationName + " → " + data.foretellDetail[i].endStationName +
+                "\n已到达 " + data.foretellDetail[i].vehicleStationName +
+                "\n还有 " + data.foretellDetail[i].distanceStationNumber + " 个站到达 " + data.queryStationName +
+                "\n-------------------------------------------------------------------------------------------\n";
+        }
+        console.log(txt);
     }
-    console.log(txt);
+    else {
+        console.log("找不到您需要的到站数据");
+    }
 });
